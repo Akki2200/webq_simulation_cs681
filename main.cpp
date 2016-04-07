ec8fcab2d8f2248b73872ed052143d7aed17a7dc
Akki2200 <Aakash2200@gmail.com>
2016-04-07 19:42:24 +0530

    Testing simulation with Priority Queue of events
    Created a buffer at each station (user, generator, waiting, checker,
    server), and a global events priority queue.
    Tested the simulation loop.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-
 #include <iostream>
 #include <queue>
+#include <cfloat>
+#include <climits>
 using namespace std;
 
+#define WAITING_TIME 5
+#define SERVER_TIME 5
 
 enum RequestType{
 	USER,
@@ -13,6 +16,9 @@ enum RequestType{
 };
 
 class Request{
+
+public:
+
 	int id;					//unique id of the request
 	float arrival_time;		//arrival time of the request
 	float next_event_time;	//this request will occur at next_event_time
@@ -20,13 +26,14 @@ class Request{
 							//  imminent) be processed first
 	RequestType type;
 
-public:
+
 	Request(int id, float arrival_time){
 		this->id = id;
 		this->arrival_time = arrival_time;
 		this->next_event_time = arrival_time;
 		this->type = USER;
 	}
+
 
 	//less than
 	friend bool operator< (const Request& lhs, const Request& rhs){
@@ -36,6 +43,11 @@ public:
 	//greater than
 	friend bool operator> (const Request& lhs, const Request& rhs){
 		return lhs.next_event_time > rhs.next_event_time;
+	}
+
+	//not equalto
+	friend bool operator!= (const Request& lhs, const Request& rhs){
+		return lhs.next_event_time != rhs.next_event_time;
 	}
 
 	//print request description
@@ -51,24 +63,111 @@ public:
 
 int main(int argc,char** argv){
 
+	priority_queue< Request, vector<Request>, greater<Request> > 
+	                                  user, generator, waiting, checker, server;
+	priority_queue< Request, vector<Request>, greater<Request> > events;
 
-	priority_queue< Request, vector<Request>, greater<Request> > clients;
+	Request max(INT_MAX, FLT_MAX);
 
-	//fill the queue
+	// create users
 	for (int i = 100; i > 0; --i)
 	{
-		Request new_client(i, (float)i/10);
-		clients.push(new_client);
+		Request new_user(i, i/10.0f);
+		user.push(new_user);
+		events.push(new_user);
+	}
+	int line_number = 1;
+
+	while(!events.empty()){
+		Request next_event = events.top();
+		events.pop();
+		switch(next_event.type) {
+			case USER:
+				cout << "I am a user" << line_number++ << endl;
+				cout << next_event << endl;
+				if(user.top()!=next_event){
+					cout << "Error processing USER event" << endl;
+					cout << next_event << endl;
+					return INT_MIN;
+				}
+				user.pop();
+				next_event.type = GENERATOR;
+				generator.push(next_event);
+				events.push(next_event);
+				break;
+
+			case GENERATOR:
+				cout << "I am a generator" << line_number++ << endl;
+				cout << next_event << endl;
+				if(generator.top()!=next_event){
+					cout << "Error processing GENERATOR event" << endl;
+					cout << next_event << endl;
+					return INT_MIN;
+				}
+				generator.pop();
+				next_event.type = WAITING;
+				next_event.next_event_time += WAITING_TIME;
+				waiting.push(next_event);
+				events.push(next_event);
+				break;
+
+			case WAITING:
+				cout << "I am a waiting" << line_number++ << endl;
+				cout << next_event << endl;
+				if(waiting.top()!=next_event){
+					cout << "Error processing WAITING event" << endl;
+					cout << next_event << endl;
+					return INT_MIN;
+				}
+				waiting.pop();
+				next_event.type = CHECKER;
+				checker.push(next_event);
+				events.push(next_event);
+				break;
+
+			case CHECKER:
+				cout << "I am a checker" << line_number++ << endl;
+				cout << next_event << endl;
+				if(checker.top()!=next_event){
+					cout << "Error processing CHECKER event" << endl;
+					cout << next_event << endl;
+					return INT_MIN;
+				}
+				checker.pop();
+				next_event.type = SERVER;
+				next_event.next_event_time += SERVER_TIME;
+				server.push(next_event);
+				events.push(next_event);
+				break;
+
+			case SERVER:
+				cout << "I am a server" << line_number++ << endl;
+				cout << next_event << endl;
+				if(server.top()!=next_event){
+					cout << "Error processing SERVER event" << endl;
+					cout << next_event << endl;
+					return INT_MIN;
+				}
+				server.pop();
+				break;
+
+			default:
+				cout << "Error processing event's RequestType" << endl;
+				return INT_MIN;
+		}
 	}
 
 	//print the queue
 	for (int i = 0; i < 100 ; ++i)
 	{
-		// Request r1 = clients.front(); clients.pop();
-		// Request r2 = clients.front(); clients.pop();
+		// Request r1 = genera.front(); generator.pop();
+		// Request r2 = generator.front(); generator.pop();
 		// cout<< (r1 < r2) <<endl;
-		cout << clients.top() << endl;
-		clients.pop();
+
+		// cout << user.front() << endl;
+		// Request new_client(generator.top().id,generator.top().arrival_time);
+		// waiting_room_q.push(new_client);
+		// user.pop();
 	}	
 	return 0;
 }
